9f3c3d4b6e700ff56b1464671359aab13d797dda
anoop <anoop@DESKTOP-M5RIN9L>
2023-06-21 10:53:15 -0400

    Add project files.

---
+++ eTickets/Models/Actor_Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Models
+{
+    public class Actor_Movie
+    {
+        public int Movie_Id { get; set; }
+
+        public Movie Movie { get; set; }
+        public int Actor_Id { get; set; }
+
+        public Actor Actor { get; set; }
+    }
+}
